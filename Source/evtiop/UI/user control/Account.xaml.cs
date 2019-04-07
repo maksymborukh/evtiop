68d491863294c112e7719b55f4bb3b4bd0953b9e
Maksym Borukh <maksboruh@gmail.com>
2019-04-07 22:37:52 +0300

    add little change to account page

--- Source/evtiop/UI/user control/Account.xaml.cs
+++ Source/evtiop/UI/user control/Account.xaml.cs
@@ -29,22 +29,31 @@ namespace UI.user_control
         {
             EditPage.Visibility = Visibility.Visible;
             PersonalDataPage.IsEnabled = false;
+            Image image = (Image)sender;
+            EditMessageTextBlock.Text = image.Tag.ToString();
         }
 
         private void Image_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            EditImagePage.Visibility = Visibility.Visible;
+            PersonalDataPage.IsEnabled = false;
         }
 
         private void SaveChange_Click(object sender, RoutedEventArgs e)
         {
-
+            //todo save button action in account page
         }
 
         private void CancelChange_Click(object sender, RoutedEventArgs e)
         {
+            EditImagePage.Visibility = Visibility.Collapsed;
             EditPage.Visibility = Visibility.Collapsed;
             PersonalDataPage.IsEnabled = true;
+        }
+
+        private void Upload_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
