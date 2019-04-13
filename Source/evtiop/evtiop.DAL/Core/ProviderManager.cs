1be12a02077d80ad4cf83f1e2dc81eceba950d0c
Maksym Borukh <maksboruh@gmail.com>
2019-04-13 13:16:29 +0300

    add provider manager and configuration setting:

---
+++ Source/evtiop/evtiop.DAL/Core/ProviderManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evtiop.DAL.Core
+{
+    public class ProviderManager
+    {
+        #region Property
+        public string ProviderName { get; set; }
+
+        public DbProviderFactory Factory
+        {
+            get
+            {
+                DbProviderFactory factory = DbProviderFactories.GetFactory(ProviderName);
+                return factory;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public ProviderManager()
+        {
+            ProviderName = ConfigurationSetting.GetProviderName(ConfigurationSetting.DefaultConnection);
+        }
+
+        public ProviderManager(string providerName)
+        {
+            ProviderName = providerName;
+        }
+        #endregion
+    }
+}
---
+++ Source/evtiop/evtiop.DAL/Core/SqlHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evtiop.DAL.Core
+{
+    public class SqlHelper
+    {
+        private string ConnectionString { get; set; }
+
+        public SqlHelper(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public void CloseConnection(SqlConnection connection)
+        {
+
+        }
+    }
+}
